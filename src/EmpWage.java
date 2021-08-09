f4db0f5a9e7fd67c8b0fa61076cfcbb12fb7cb00
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-09 20:45:45 +0530

    added UC 6

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -10,7 +10,8 @@ public class EmpWage {
 			System.out.println("1. To check Employee is Present or Not \n"
 					+ "2. To Calculate Daily Wage of Employee\n"
 					+ "3. To Calculate Part Time Wage of Employee\n"
-					+ "4. To Calculate Wages for a Month\n");
+					+ "4. To Calculate Wages for a Month\n"
+					+ "5. To Calculate Wages for 100 hours or 20 Days");
 			// Use Case - 1
 			int a = sc.nextInt();
 			// Use Case - 4
@@ -21,9 +22,9 @@ public class EmpWage {
 				double empCheck = Math.floor(random* 10) % 2;
 			
 				if(empCheck == 1)
-					System.out.println("Employee is Present");
+					System.out.println("Employee is Present\n");
 				else
-					System.out.println("Employee is not Present");
+					System.out.println("Employee is not Present\n");
 				break;
 			
 			case 2:	
@@ -37,6 +38,10 @@ public class EmpWage {
 			case 4:
 					// Use Case - 5
 					System.out.println("Part Time Wage of Employee is " + (WagePerHour * FullDay) * totalDays + "\n");
+					break;
+			case 5:
+					// Use Case - 6
+					System.out.println("Part Time Wage of Employee is " + (WagePerHour * (FullDay-3)) * totalDays + "\n");
 					break;
 			}
 		
