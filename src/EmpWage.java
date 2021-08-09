be391efc1976a920a336c0baf8fb61a2df47c8cf
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-09 16:48:03 +0530

    Added Employee Attendance

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,4 +1,17 @@
 
 public class EmpWage {
-	
+	public static void main(String[] args) {
+		int FullDay = 8;
+		int WagePerHour= 20;
+		int PartTime = 8;
+		
+		// Use Case - 1
+		double random = Math.random();
+		System.out.println(random);
+		double empCheck = Math.floor(random* 10) % 2;
+		if(empCheck == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is not Present");
+	}
 }
