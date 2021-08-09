7f1c40af876dd4f00e76e58d9278f2457271e85b
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-09 20:17:35 +0530

    added switch method

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,26 +1,38 @@
-
+import java.util.*;
 public class EmpWage {
 	public static void main(String[] args) {
 		int FullDay = 8;
 		int WagePerHour= 20;
 		int PartTime = 8;
-		
-		// Use Case - 1
-		double random = Math.random();
-		System.out.println(random);
-		double empCheck = Math.floor(random* 10) % 2;
-		if(empCheck == 1)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is not Present");
-		
-		if(empCheck == 1) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("1. To check Employee is Present or Not \n"
+					+ "2. To Calculate Daily Wage of Employee\n"
+					+ "3. To Calculate Part Time Wage of Employee\n");
+			// Use Case - 1
+			int a = sc.nextInt();
+			switch(a) {
+			case 1:
+				double random = Math.random();
+				System.out.println(random);
+				double empCheck = Math.floor(random* 10) % 2;
 			
-			// Use Case - 2
-			System.out.println("Daily Wage of Employee is " + Integer.toString(WagePerHour*FullDay));
+				if(empCheck == 1)
+					System.out.println("Employee is Present");
+				else
+					System.out.println("Employee is not Present");
+				break;
 			
-			// Use Case - 3
-			System.out.println("Part Time Wage of Employee is " + Integer.toString(WagePerHour*PartTime));
-		}
+			case 2:	
+					// Use Case -2 
+					System.out.println("Daily Wage of Employee is " + Integer.toString(WagePerHour * FullDay) + "\n");
+					break;
+			case 3:
+					// Use Case - 3
+					System.out.println("Part Time Wage of Employee is " + Integer.toString(WagePerHour * PartTime)+ "\n");
+					break;
+			}
+		
 	}
+		}
 }
